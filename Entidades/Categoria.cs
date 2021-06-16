f5a080980e50a7318ca6c95cff83c5e838338268
Matias Flori <matyflori@hotmail.com>
2021-06-16 12:00:41 -0300

    V 0.6.1 Solamente agregue la entidad categoria

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        //Parametros
+        private string nombre;
+        private string codigo;
+
+        //Constructores
+        public Categoria()
+        {
+        }
+
+
+        //Set & Get
+        public string Codigo
+        {
+            set { codigo = value; }
+            get { return codigo; }
+        }
+        public string Nombre
+        {
+            set { nombre = value; }
+            get { return nombre; }
+        }
+    }
+}
